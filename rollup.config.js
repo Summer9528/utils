63ee4fd78a4bfa22afb441acfa711eafaeab7c19
Summer <Summere9528@foxmail.com>
2022-12-28 16:51:36 +0800

    style

--- rollup.config.js
+++ rollup.config.js
@@ -4,38 +4,37 @@ import alias from '@rollup/plugin-alias'
 import path from 'path'
 
 export const builds = {
-    "esm": {
-        entry: "src/index.js",
-        output: name => `dist/${name}.js`,
-        format: "esm",
-        plugins: []
-    }
+  esm: {
+    entry: 'src/index.js',
+    output: name => `dist/${name}.js`,
+    format: 'esm',
+    plugins: []
+  }
 }
 const getConfig = (name) => {
-    const opts = builds[name]
-    const config = {
-        input: opts.entry,
-        external: opts.external || [],
-        output: {
-            file: opts.output(`bundle-${name}`),
-            format: opts.format,
-            name: opts.name || 'utils'
-        },
-        plugins: [
-            alias({
-                entries: [
-                    { find: '@', replacement: path.resolve('src') }
-                ]
-            })
+  const opts = builds[name]
+  const config = {
+    input: opts.entry,
+    external: opts.external || [],
+    output: {
+      file: opts.output(`bundle-${name}`),
+      format: opts.format,
+      name: opts.name || 'utils'
+    },
+    plugins: [
+      alias({
+        entries: [
+          { find: '@', replacement: path.resolve('src') }
         ]
-            .concat((opts.plugins || []))
-            .concat([
-                babel({
-                    exclude: ['node_modules/**']
-                })
-            ])
-    }
-    return config
+      })
+    ]
+      .concat((opts.plugins || []))
+      .concat([
+        babel({
+          exclude: ['node_modules/**']
+        })
+      ])
+  }
+  return config
 }
 export default Object.keys(builds).map(getConfig)
-
